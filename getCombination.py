a199603365a7415118d372b6908103a4d958dc4a
User <user@yuzanoMacBook-Pro.local>
2022-12-06 04:17:27 +0900

    add new utilities

---
+++ getCombination.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+# 重複なし組み合わせ、順列
+
+def getCombination(itr , num :int, distinct: bool=True) ->list:
+    combs = []
+    itrLen = len(itr)
+    n = 1 << itrLen
+    for i in range(n):
+        bit_str = format(i,'b')
+        if bit_str.count('1') != num: continue
+        bit_str = bit_str.zfill(itrLen)
+        comb = []
+        for j in range(itrLen):
+            if bit_str[j] == '1': comb.append(itr[j])
+        if len(comb) == num:
+            if distinct: comb.sort()
+            if comb in combs:continue
+            combs.append(comb)
+    return combs
+
+# 重複なし組み合わせ、順列
+
+def getDuplicatedComb(itr , num :int,distinct: bool =True) -> list:
+    combs = []
+    itrLen = len(itr)
+    bitLen = itrLen*num
+    n = 1 << bitLen
+    for i in range(n):
+        bit_str = format(i,'b')
+        if bit_str.count('1') != num: continue
+        bit_str = bit_str.zfill(bitLen)
+        comb = []
+        for j in range(bitLen):
+            if bit_str[j] == '1': comb.append(itr[(j%itrLen)])
+        if len(comb) == num:
+            if distinct: comb.sort()
+            if comb in combs:continue
+            combs.append(comb)
+    return combs
+
+print(len(getCombination([1,2,3,4,5,6,7,8,9,10],6)))
+            
--- nextPermutation.py
+++ nextPermutation.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-def nextPermutation(nums: list) -> list:
+def nextPermutation(itr) -> list:
     def reverse(arr :list,start :int):
         arrLen = len(arr)
         j = arrLen-1
@@ -9,19 +9,19 @@ def nextPermutation(nums: list) -> list:
             j-=1
             if i >= j:break
     
-    def swap(arr :list, first :int, second :int):
+    def swap(arr :list, first, second):
         a = arr[first]
         b = arr[second]
         arr[second] = a
         arr[first] = b
         
-    numsLen = len(nums)
-    if numsLen <= 1:return nums
+    numsLen = len(itr)
+    if numsLen <= 1:return itr
     i = numsLen - 1
-    while i > 0 and nums[i] <= nums[i-1]:i-=1
+    while i > 0 and itr[i] <= itr[i-1]:i-=1
     if i > 0:
         j = i
-        while j < numsLen and nums[j] > nums[i-1]:j+=1
-        swap(nums,i-1,j-1)
-    reverse(nums,i)
+        while j < numsLen and itr[j] > itr[i-1]:j+=1
+        swap(itr,i-1,j-1)
+    reverse(itr,i)
 
